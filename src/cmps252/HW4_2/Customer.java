97668541d89f3062a45ceee6a5b1775704732990
Dhia Eddine Nini <dxn00@mail.aub.edu>
2020-10-27 14:55:15 +0100

    Finished implementing Customer.java and FileParser.java classes - By Dhia Eddine Nini

--- src/cmps252/HW4_2/Customer.java
+++ src/cmps252/HW4_2/Customer.java
@@ -1,6 +1,128 @@
 package cmps252.HW4_2;
 
 public class Customer {
-	//TODO add your code here	
+	public String FirstName;
+	public String LastName;
+	public String Company;
+	public String Address;
+	public String City;
+	public String County;
+	public String State;
+	public String ZIP;
+	public String Phone;
+	public String Fax;
+	public String Email;
+	public String Web;
 	
+	public Customer(String firstName, String lastName, String company, String address, String city, String county,
+			String state, String zip, String phone, String fax, String email, String web) {
+		FirstName = firstName;
+		LastName = lastName;
+		Company = company;
+		Address = address;
+		City = city;
+		County = county;
+		State = state;
+		ZIP = zip;
+		Phone = phone;
+		Fax = fax;
+		Email = email;
+		Web = web;
+	}
+	
+	public String getFirstName() {
+		return FirstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+	
+	public String getLastName() {
+		return LastName;
+	}
+	
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	
+	public String getCompany() {
+		return Company;
+	}
+	
+	public void setCompany(String company) {
+		Company = company;
+	}
+	
+	public String getAddress() {
+		return Address;
+	}
+	
+	public void setAddress(String address) {
+		Address = address;
+	}
+	
+	public String getCity() {
+		return City;
+	}
+	
+	public void setCity(String city) {
+		City = city;
+	}
+	
+	public String getCounty() {
+		return County;
+	}
+	
+	public void setCounty(String county) {
+		County = county;
+	}
+	
+	public String getState() {
+		return State;
+	}
+	
+	public void setState(String state) {
+		State = state;
+	}
+	
+	public String getZIP() {
+		return ZIP;
+	}
+	
+	public void setZIP(String zip) {
+		ZIP = zip;
+	}
+	
+	public String getPhone() {
+		return Phone;
+	}
+	
+	public void setPhone(String phone) {
+		Phone = phone;
+	}
+	
+	public String getFax() {
+		return Fax;
+	}
+	
+	public void setFax(String fax) {
+		Fax = fax;
+	}
+	
+	public String getEmail() {
+		return Email;
+	}
+	
+	public void setEmail(String email) {
+		Email = email;
+	}
+	
+	public String getWeb() {
+		return Web;
+	}
+	
+	public void setWeb(String web) {
+		Web = web;
+	}
 }
--- src/cmps252/HW4_2/FileParser.java
+++ src/cmps252/HW4_2/FileParser.java
@@ -1,5 +1,44 @@
 package cmps252.HW4_2;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class FileParser {
-	//TODO add your code here
+	public static List<Customer> getCustomers(String fileName) throws FileNotFoundException {
+		File file = new File(fileName);
+		Scanner scanner = new Scanner(file);
+		
+		List<Customer> customers = new ArrayList<Customer>();
+		
+		scanner.nextLine();	// to ignore first line
+		while(scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+			String[] fields = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+			
+			// remove quotations:
+			for (int i = 0; i < fields.length; i++) {
+				fields[i] = fields[i].replace("\"", "");
+			}
+			
+			Customer customer = new Customer(
+					fields[0], 
+					fields[1], 
+					fields[2], 
+					fields[3], 
+					fields[4], 
+					fields[5], 
+					fields[6], 
+					fields[7], 
+					fields[8], 
+					fields[9], 
+					fields[10], 
+					fields[11]
+			);
+			customers.add(customer);
+		}
+		
+		return customers;
+	}
 }
